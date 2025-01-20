96f1b407976a8a8e740e8d4c5c4cf962bc3899cf
Chief <75649719+sosa223@users.noreply.github.com>
2025-01-20 18:37:50 +0000

    new

---
+++ .ccls-cache/@home@runner@workspace/PersonalInfo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    char name[] = "Robert Hall";
+    char birthdate[] = "2/27/1949";
+    char workPhone[] = "000-444-0101";
+    char cellPhone[] = "000-444-0189";
+    printf("%s\n", name);   
+    printf("%s\n", birthdate); 
+    printf("work %s\n", workPhone); 
+    printf("cell %s\n", cellPhone); 
+
+    return 0;
+}
